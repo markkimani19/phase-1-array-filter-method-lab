a64ce4f147c007989e239d0b26b550ee7e2a717d
mark <mark.kimani3@student.moringaschool.com>
2025-03-31 04:02:34 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+// index.js
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  // Export the functions if needed for testing
+  module.exports = { findMatching, fuzzyMatch, matchName };
